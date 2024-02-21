db29bf2d50d69837073c338abc1513800a3f1b64
Govind Wakure <53077733+govindwakure@users.noreply.github.com>
2024-02-20 21:34:03 -0800

    Add files via upload
    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+```
+C:\Program Files\MySQL\MySQL Server 5.7\bin>mysqldump -u root -p dbname.sql > "path\dbname.sql"
+
+```pip install requirements.txt ```
---
+++ config.py
@@ -0,0 +1,8 @@
+# config.txt
+
+SECRET_KEY='your_secret_key_here'
+DEBUG=True
+DATABASE_URI='mysql+mysqlconnector://root:password@localhost/internship'
+ADMIN_EMAIL = 'skillhivedumy@gmail.com'
+ADMIN_EMAIL_PASSWORD = 'fwgrugbsykerdtaw'
+EMAIL_PORT = 587
